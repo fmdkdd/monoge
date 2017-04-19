12440b4e348b81365590cada9b901fc92d2ad07e
fmdkdd <fmdkdd@gmail.com>
2017-04-19 14:59:40 +0200

    Actually testing something

--- plugin-tuto/com.packtpub.e4.junit.plugin/src/com/packtpub/e4/junit/plugin/UITest.java
+++ plugin-tuto/com.packtpub.e4.junit.plugin/src/com/packtpub/e4/junit/plugin/UITest.java
@@ -1,19 +1,30 @@
 package com.packtpub.e4.junit.plugin;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.swtbot.swt.finder.SWTBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class UITest {
 
+  private static SWTBot bot;
+
+  @BeforeClass
+  public static void beforeClass() {
+    bot = new SWTBot();
+  }
+
   @Test
   public void testUI() {
-    SWTWorkbenchBot bot = new SWTWorkbenchBot();
     SWTBotShell[] shells = bot.shells();
     for (SWTBotShell s : shells) {
       if (s.isVisible()) {
@@ -25,7 +36,11 @@ public class UITest {
 
   @Test
   public void createProject() {
-    SWTWorkbenchBot bot = new SWTWorkbenchBot();
+    String projectName = "SWTBot Test Project";
+
+    assertFalse(ResourcesPlugin.getWorkspace().getRoot().getProject(projectName)
+        .exists());
+
     bot.menu("File").menu("New").menu("Project...").click();
     SWTBotShell shell = bot.shell("New Project");
     shell.activate();
@@ -33,5 +48,13 @@ public class UITest {
     bot.button("Next >").click();
     bot.textWithLabel("Project name:").setText("SWTBot Test Project");
     bot.button("Finish").click();
+
+    assertTrue(ResourcesPlugin.getWorkspace().getRoot().getProject(projectName)
+        .exists());
+  }
+
+  @AfterClass
+  public static void sleep() {
+    bot.sleep(2000);
   }
 }
