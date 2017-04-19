224163293aa190929ae0fad0fef31a9292445b34
fmdkdd <fmdkdd@gmail.com>
2017-04-19 11:06:42 +0200

    Using the EventBroker

--- plugin-tuto/com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
+++ plugin-tuto/com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Hello.java
@@ -6,6 +6,7 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
@@ -40,6 +41,12 @@ public class Hello {
     if (selection != null) {
       label.setText(selection.toString());
     }
+  }
+
+  @Inject
+  @Optional
+  public void receiveEvent(@UIEventTopic("rainbow/color") String color) {
+    label.setText(color);
   }
 
 }
--- plugin-tuto/com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Rainbow.java
+++ plugin-tuto/com.packtpub.e4.application/src/com/packtpub/e4/application/parts/Rainbow.java
@@ -3,8 +3,10 @@ package com.packtpub.e4.application.parts;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ListViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
@@ -15,12 +17,17 @@ public class Rainbow {
   @Inject
   private ESelectionService selectionService;
 
+  @Inject
+  private IEventBroker eventBroker;
+
   @PostConstruct
   public void create(Composite parent) {
     ListViewer lv = new ListViewer(parent, SWT.NONE);
     lv.setContentProvider(new ArrayContentProvider());
     lv.addSelectionChangedListener((event) -> {
-      selectionService.setSelection(event.getSelection());
+      Object color = ((IStructuredSelection) event.getSelection())
+          .getFirstElement();
+      eventBroker.post("rainbow/color", color);
     });
     lv.setInput(colors);
   }
