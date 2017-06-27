4c1f49d88c991186c45ddf960ef6420ac9f0ef49
fmdkdd <fmdkdd@gmail.com>
2017-06-27 11:22:31 +0200

    Test EcoreUtil.delete behavior

--- emfviews-test/src/emfviews/test/PackageComposition.java
+++ emfviews-test/src/emfviews/test/PackageComposition.java
@@ -1,12 +1,19 @@
 package emfviews.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.junit.Before;
 import org.junit.Test;
@@ -101,5 +108,32 @@ public class PackageComposition {
     EPackage PA_ = EcoreUtil.copy(PA);
     // But how to set the super package?
     // PA_.getESuperPackage().getESubpackages().add(PA);
+  }
+
+  @Test
+  public void testEcoreRemovingOppositeReference() {
+    // When we remove a part of a bidirectional reference in an Ecore metamodel,
+    // is the opposite removed?
+
+    Resource r = new ResourceSetImpl()
+        .getResource(URI.createURI("resources/metamodels/minimalref.ecore", true), true);
+    EPackage p = (EPackage) r.getContents().get(0);
+    EClass A = (EClass) p.getEClassifiers().get(0);
+    EClass B = (EClass) p.getEClassifiers().get(1);
+    EReference manyB = (EReference) A.getEStructuralFeature("manyB");
+    EReference parentA = (EReference) B.getEStructuralFeature("parentA");
+
+    assertNotNull(manyB);
+    assertEquals(parentA, manyB.getEOpposite());
+    assertNotNull(parentA);
+    assertEquals(manyB, parentA.getEOpposite());
+
+    EcoreUtil.delete(manyB);
+
+    assertNull(A.getEStructuralFeature("manyB"));
+    assertNull(parentA.getEOpposite());
+
+    // Yes, the opposite is removed if we use EcoreUtil.delete, but not if we
+    // use Ecore.remove.
   }
 }
