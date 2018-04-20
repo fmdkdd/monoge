20bff64e85b08f063d0189edc7f8ecdb28386edb
fmdkdd <fmdkdd@0xc0de.fr>
2018-04-20 17:52:01 +0200

    Testing OCL on views

---
+++ neoemf-integration/benchmarks/src/OCLQuery.java
@@ -0,0 +1,112 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.atlanmod.emfviews.core.EmfViewsFactory;
+import org.atlanmod.emfviews.virtuallinks.VirtualLinksPackage;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.gmt.modisco.java.emf.JavaPackage;
+import org.eclipse.m2m.atl.emftvm.impl.resource.EMFTVMResourceFactoryImpl;
+import org.eclipse.ocl.OCL;
+import org.eclipse.ocl.ParserException;
+import org.eclipse.ocl.Query;
+import org.eclipse.ocl.ecore.EcoreEnvironmentFactory;
+import org.eclipse.ocl.expressions.OCLExpression;
+import org.eclipse.ocl.helper.OCLHelper;
+import org.eclipse.rmf.reqif10.ReqIF10Package;
+import org.eclipse.rmf.reqif10.serialization.ReqIF10ResourceFactoryImpl;
+import org.eclipse.uml2.uml.UMLPackage;
+import org.eclipse.uml2.uml.internal.resource.UMLResourceFactoryImpl;
+
+import fr.inria.atlanmod.neoemf.data.PersistenceBackendFactoryRegistry;
+import fr.inria.atlanmod.neoemf.data.blueprints.BlueprintsPersistenceBackendFactory;
+import fr.inria.atlanmod.neoemf.data.blueprints.util.BlueprintsURI;
+import fr.inria.atlanmod.neoemf.resource.PersistentResourceFactory;
+import trace.TracePackage;
+import traceneoemf.TraceneoemfPackage;
+import virtuallinksneoemf.VirtuallinksneoemfPackage;
+
+public class OCLQuery {
+
+  static OCL ocl;
+  static OCLHelper oclHelper;
+  static OCLExpression<EClassifier> expression;
+
+  static void setUp() throws ParserException {
+    // Init EMF
+    {
+      Map<String, Object> map = Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap();
+      map.put("eviewpoint", new EmfViewsFactory());
+      map.put("eview", new EmfViewsFactory());
+      map.put("xmi", new XMIResourceFactoryImpl());
+      map.put("ecore", new EcoreResourceFactoryImpl());
+      map.put("reqif", new ReqIF10ResourceFactoryImpl());
+      map.put("uml", new UMLResourceFactoryImpl());
+      map.put("emftvm", new EMFTVMResourceFactoryImpl());
+
+      // Load metamodels
+      ReqIF10Package.eINSTANCE.eClass();
+      UMLPackage.eINSTANCE.eClass();
+      JavaPackage.eINSTANCE.eClass();
+      TracePackage.eINSTANCE.eClass();
+      TraceneoemfPackage.eINSTANCE.eClass();
+      VirtualLinksPackage.eINSTANCE.eClass();
+      VirtuallinksneoemfPackage.eINSTANCE.eClass();
+
+      PersistenceBackendFactoryRegistry.register(BlueprintsURI.SCHEME,
+                                                 BlueprintsPersistenceBackendFactory.getInstance());
+      Resource.Factory.Registry.INSTANCE.getProtocolToFactoryMap()
+      .put(BlueprintsURI.SCHEME, PersistentResourceFactory.getInstance());
+    }
+
+
+  }
+
+
+  static Resource resource;
+
+  static void benchQuery(URI uri, EObject context) throws Exception {
+    // Need to recreate the query since otherwise it does not get re-evaluated for subsequent resources
+    // Init OCL query
+    OCL ocl = OCL.newInstance(EcoreEnvironmentFactory.INSTANCE);
+    OCLHelper oclHelper = ocl.createOCLHelper();
+    oclHelper.setContext(context);
+    expression = oclHelper.createQuery("Log.allInstances()");
+    Query<EClassifier, EClass, EObject> query = ocl.createQuery(expression);
+
+    Util.time("Load resource", () -> {
+      resource = Util.loadResource(uri);
+    });
+    Util.time("Evaluate query", () -> {
+      Object res = query.evaluate(resource.getContents().get(0));
+      System.out.printf("Result size: %s\n", ((HashSet) res).size());
+    });
+    Util.time("Unload resource", () -> { resource.unload(); });
+  }
+
+  public static void main(String[] args) throws Exception {
+    setUp();
+
+    // Bench
+    final int[] sizes = {10, 100, 1000, 10000};
+/*
+    for (int s : sizes) {
+      Util.time(String.format("OCL allInstances query for XMI model of size %d", s), () -> {
+        benchQuery(Util.resourceURI("/models/java-trace/%d.xmi", s));
+      });
+    }
+*/
+    for (int s : sizes) {
+      Util.time(String.format("OCL allInstances query for NeoEMF model of size %d", s), () -> {
+        benchQuery(Util.resourceURI("/models/neoemf-trace/%d.graphdb", s), TraceneoemfPackage.eINSTANCE.getTrace());
+      });
+    }
+  }
+
+}
