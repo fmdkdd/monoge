b85ed0df0dbae863f257bbb8d8fc87ac6f1145c0
fmdkdd <fmdkdd@0xc0de.fr>
2019-03-25 18:20:28 +0100

    Metamodels for Epsilon models

--- splitting-queries/epsilon-test/resources/matching.ecl
+++ splitting-queries/epsilon-test/resources/matching.ecl
@@ -1,6 +1,13 @@
-rule foo
+/*rule rows
+match a : ECORE!EClass with b : CSV!Row {
+  compare {
+    return a.name = b.name;
+  }
+}*/
+
+rule eclasses
 match a : CSV!Row with b : ECORE!EClass {
   compare {
-    return a.name = b.name;  
+    return a.name = b.name;
   }
 }
--- splitting-queries/epsilon-test/src/TestViewWithEpsilonObject.java
+++ splitting-queries/epsilon-test/src/TestViewWithEpsilonObject.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
@@ -7,9 +9,11 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import org.atlanmod.emfviews.core.EpsilonResource;
 import org.atlanmod.emfviews.core.ViewResource;
+import org.atlanmod.emfviews.virtuallinks.VirtualLinksPackage;
 
 public class TestViewWithEpsilonObject {
   static String here = new File(".").getAbsolutePath();
@@ -18,9 +22,16 @@ public class TestViewWithEpsilonObject {
     return URI.createFileURI(here + relativePath);
   }
 
+  static Stream<EObject> contentsStream(Resource r) {
+    Iterable<EObject> contents = () -> r.getAllContents();
+    return StreamSupport.stream(contents.spliterator(), true);
+  }
+
   public static void main(String[] args) throws IOException {
     EcorePackage.eINSTANCE.eClass();
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+    VirtualLinksPackage.eINSTANCE.eClass();
+    Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
 
     Resource.Factory epsRF = new Resource.Factory() {
       @Override
@@ -36,13 +47,34 @@ public class TestViewWithEpsilonObject {
 
     r.load(null);
 
-    for (EObject o : r.getContents()) {
+    Iterable<EObject> eClasses = () -> contentsStream(r)
+      .filter(o -> o.eClass().getName().equals("EClass"))
+      .iterator();
+
+    for (EObject o : eClasses) {
       {
         EStructuralFeature f = o.eClass().getEStructuralFeature("name");
         System.out.println(o.eGet(f));
       }
       {
-        EStructuralFeature f = o.eClass().getEStructuralFeature("foo");
+        EStructuralFeature f = o.eClass().getEStructuralFeature("rows");
+        if (f != null) {
+          System.out.println(o.eGet(f));
+        }
+      }
+    }
+
+    Iterable<EObject> rows = () -> contentsStream(r)
+      .filter(o -> o.eClass().getName().equals("Row"))
+      .iterator();
+
+    for (EObject o : rows) {
+      {
+        EStructuralFeature f = o.eClass().getEStructuralFeature("name");
+        System.out.println(o.eGet(f));
+      }
+      {
+        EStructuralFeature f = o.eClass().getEStructuralFeature("eclasses");
         if (f != null) {
           System.out.println(o.eGet(f));
         }
