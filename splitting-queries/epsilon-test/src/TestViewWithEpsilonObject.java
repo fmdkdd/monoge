6cb89352031db674d9683e533501728d8c14b7c1
fmdkdd <fmdkdd@0xc0de.fr>
2019-03-27 17:03:45 +0100

    Run EOL query on view

--- splitting-queries/epsilon-test/src/TestViewWithEpsilonObject.java
+++ splitting-queries/epsilon-test/src/TestViewWithEpsilonObject.java
@@ -1,15 +1,19 @@
 import java.io.File;
-import java.io.IOException;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.epsilon.common.parse.problem.ParseProblem;
+import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
+import org.eclipse.epsilon.eol.EolModule;
+import org.eclipse.epsilon.eol.execute.operations.EolOperationFactory;
 
 import org.atlanmod.emfviews.core.EpsilonResource;
 import org.atlanmod.emfviews.core.ViewResource;
@@ -27,7 +31,7 @@ public class TestViewWithEpsilonObject {
     return StreamSupport.stream(contents.spliterator(), true);
   }
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     EcorePackage.eINSTANCE.eClass();
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
     VirtualLinksPackage.eINSTANCE.eClass();
@@ -46,6 +50,8 @@ public class TestViewWithEpsilonObject {
     ViewResource r = new ViewResource(resourceURI("/resources/test.eview"));
 
     r.load(null);
+
+    // Check access to virtual features through EMF API
 
     Iterable<EObject> eClasses = () -> contentsStream(r)
       .filter(o -> o.eClass().getName().equals("EClass"))
@@ -80,5 +86,28 @@ public class TestViewWithEpsilonObject {
         }
       }
     }
+
+    // Run EOL query on the view
+
+    EolModule module = new EolModule();
+    module.parse("VIEW!Row.all.eclasses.name.println();");
+    if (module.getParseProblems().size() > 0) {
+      System.err.println("Parse errors occured...");
+      for (ParseProblem problem : module.getParseProblems()) {
+        System.err.println(problem.toString());
+      }
+      throw new Exception("Error in parsing ECL file.  See stderr for details");
+    }
+    module.getContext().setOperationFactory(new EolOperationFactory());
+
+
+
+    // Add view
+    // Need the viewpoint as well
+    EPackage p = r.getContents().get(0).eClass().getEPackage();
+    module.getContext().getModelRepository().addModel(new InMemoryEmfModel("VIEW", r, p));
+
+    // Execute the module
+    System.out.format("EOL result: %s", module.execute());
   }
 }
