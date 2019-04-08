6ae36b041a5c75d13b1302aa7c0ca31803e5a207
fmdkdd <fmdkdd@0xc0de.fr>
2019-04-08 16:19:01 +0200

    Testing the EMF Views EMC

--- splitting-queries/epsilon-test/src/TestEMFViewsEMC.java
+++ splitting-queries/epsilon-test/src/TestEMFViewsEMC.java
@@ -1,34 +1,27 @@
 import java.io.File;
-import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
+import java.io.IOException;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
-import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.EcorePackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.epsilon.common.parse.problem.ParseProblem;
-import org.eclipse.epsilon.emc.emf.InMemoryEmfModel;
+import org.eclipse.epsilon.emc.emfviews.EMFViewsModel;
 import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.execute.operations.EolOperationFactory;
 
+import org.atlanmod.emfviews.core.EmfViewsFactory;
 import org.atlanmod.emfviews.core.EpsilonResource;
 import org.atlanmod.emfviews.core.ViewResource;
 import org.atlanmod.emfviews.virtuallinks.VirtualLinksPackage;
 
-public class TestViewWithEpsilonObject {
+public class TestEMFViewsEMC {
   static String here = new File(".").getAbsolutePath();
 
   static URI resourceURI(String relativePath) {
     return URI.createFileURI(here + relativePath);
-  }
-
-  static Stream<EObject> contentsStream(Resource r) {
-    Iterable<EObject> contents = () -> r.getAllContents();
-    return StreamSupport.stream(contents.spliterator(), true);
   }
 
   public static void main(String[] args) throws Exception {
@@ -36,59 +29,20 @@ public class TestViewWithEpsilonObject {
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
     VirtualLinksPackage.eINSTANCE.eClass();
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
-
     Resource.Factory epsRF = new Resource.Factory() {
       @Override
       public Resource createResource(URI uri) {
         return new EpsilonResource(uri);
       }
     };
-
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("csv", epsRF);
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("bib", epsRF);
+    Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("eview", new EmfViewsFactory());
 
     ViewResource r = new ViewResource(resourceURI("/resources/test.eview"));
-
     r.load(null);
 
-    // Check access to virtual features through EMF API
-
-    Iterable<EObject> eClasses = () -> contentsStream(r)
-      .filter(o -> o.eClass().getName().equals("EClass"))
-      .iterator();
-
-    for (EObject o : eClasses) {
-      {
-        EStructuralFeature f = o.eClass().getEStructuralFeature("name");
-        System.out.println(o.eGet(f));
-      }
-      {
-        EStructuralFeature f = o.eClass().getEStructuralFeature("rows");
-        if (f != null) {
-          System.out.println(o.eGet(f));
-        }
-      }
-    }
-
-    Iterable<EObject> rows = () -> contentsStream(r)
-      .filter(o -> o.eClass().getName().equals("Row"))
-      .iterator();
-
-    for (EObject o : rows) {
-      {
-        EStructuralFeature f = o.eClass().getEStructuralFeature("name");
-        System.out.println(o.eGet(f));
-      }
-      {
-        EStructuralFeature f = o.eClass().getEStructuralFeature("eclasses");
-        if (f != null) {
-          System.out.println(o.eGet(f));
-        }
-      }
-    }
-
     // Run EOL query on the view
-
     EolModule module = new EolModule();
     module.parse("VIEW!Row.all.collect(r | Sequence{ r.name, r.eclasses.name }).println();");
     if (module.getParseProblems().size() > 0) {
@@ -100,12 +54,15 @@ public class TestViewWithEpsilonObject {
     }
     module.getContext().setOperationFactory(new EolOperationFactory());
 
-
-
     // Add view
     // Need the viewpoint as well
     EPackage p = r.getContents().get(0).eClass().getEPackage();
-    module.getContext().getModelRepository().addModel(new InMemoryEmfModel("VIEW", r, p));
+    EMFViewsModel m = new EMFViewsModel();
+    m.setName("VIEW");
+    m.setModelFileUri(resourceURI("/resources/test.eview"));
+    m.setMetamodelUri(p.getNsURI());
+    m.load();
+    module.getContext().getModelRepository().addModel(m);
 
     // Execute the module
     System.out.format("EOL result: %s", module.execute());
